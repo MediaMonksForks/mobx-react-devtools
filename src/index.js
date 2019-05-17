72a749989d56beeb304c85eee0b5938af74e6488
Paul Simk <paulius.simkevicius@mediamonks.com>
2019-05-17 13:46:41 +0200

    remove unused modules

--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
-import { setGlobalState } from "./globalStore"
+import { setGlobalState } from './globalStore';
 
-export { default } from "./DevTool"
-export { default as GraphControl } from "./Controls/GraphControl"
-export { default as LogControl } from "./Controls/LogControl"
-export { default as UpdatesControl } from "./Controls/UpdatesControl"
+export const configureDevtool = ({ logEnabled, logFilter }) => {
+  const config = {};
+  if (logEnabled !== undefined) {
+    config.logEnabled = Boolean(logEnabled);
+  }
 
-export const configureDevtool = ({ logEnabled, updatesEnabled, graphEnabled, logFilter }) => {
-    const config = {}
-    if (logEnabled !== undefined) {
-        config.logEnabled = Boolean(logEnabled)
-    }
-    if (updatesEnabled !== undefined) {
-        config.updatesEnabled = Boolean(updatesEnabled)
-    }
-    if (graphEnabled !== undefined) {
-        config.graphEnabled = Boolean(graphEnabled)
-    }
-    if (typeof logFilter === "function") {
-        config.logFilter = logFilter
-    }
-    setGlobalState(config)
-}
+  if (typeof logFilter === 'function') {
+    config.logFilter = logFilter;
+  }
+  setGlobalState(config);
+};
 
-export const setUpdatesEnabled = updatesEnabled => configureDevtool({ updatesEnabled })
-export const setGraphEnabled = graphEnabled => configureDevtool({ graphEnabled })
-export const setLogEnabled = logEnabled => configureDevtool({ logEnabled })
+export const setLogEnabled = logEnabled => configureDevtool({ logEnabled });
